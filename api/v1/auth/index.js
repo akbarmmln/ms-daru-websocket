c2d203202f7f7b0d826d0baafab50427dd86cb72
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-07-24 14:34:11 +0700

    template project

--- api/v1/auth/index.js
+++ api/v1/auth/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-router.post('/login', controller.getLogin);
-router.post('/pre-register', controller.getPreRegister);
-router.post('/post-register', controller.getPostReister);
+router.post('/sample', controller.getSample);
 
 module.exports = router;
