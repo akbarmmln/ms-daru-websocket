9e0cf56c6b94612335832b9b9092edae9fb16bca
Muhammad Taufik <muhammad.taufik@adira.co.id>
2024-11-25 14:00:07 +0700

    init project

--- server.js
+++ server.js
@@ -2,11 +2,45 @@
 require('dotenv').config();
 const app = require('./app');
 const logger = require('./config/logger');
+const WebSocket = require('ws');
 
-// Constants
-let PORT = process.env.PORT
+const PORT_WS = process.env.PORT_WS
 
+const clients = new Map();
+const wss = new WebSocket.Server({ host: '0.0.0.0', port: PORT_WS }, () => {
+    console.log(`WebSocket server running on ws://localhost:${PORT_WS}`);
+});
+wss.on('connection', (ws, req) => {
+    let clientId;
+
+    // Handle registration
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+
+        if (data.type === 'register') {
+            clientId = data.clientId;
+            clients.set(clientId, ws);
+            console.log(`Registered client: ${clientId}`);
+        } else if (data.type === 'message') {
+            const targetWs = clients.get(data.targetClientId);
+            if (targetWs && targetWs.readyState === WebSocket.OPEN) {
+                targetWs.send(JSON.stringify({ from: clientId, payload: data.payload }));
+            } else {
+                console.error(`Target client ${data.targetClientId} not found`);
+            }
+        }
+    });
+
+    // Cleanup on disconnect
+    ws.on('close', () => {
+        if (clientId) {
+            clients.delete(clientId);
+            console.log(`Disconnected client: ${clientId}`);
+        }
+    });
+});
+
+const PORT = process.env.PORT
 const server = app.listen(PORT, () => logger.infoWithContext(`API Server started. Listening on port:${PORT}`));
-// mqttConfig.mqtt();
 
 module.exports = server;
--- setting.js
+++ setting.js
@@ -1,24 +1,3 @@
 module.exports = {
-    mysql: {
-        hostname: process.env.MYSQL_HOSTNAME,
-        username: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD,
-        port: process.env.MYSQL_PORT,
-        dbname: process.env.MYSQL_DATABASE,
-    },
-    oss: {
-        credentials: {
-            accessKeyId: process.env.ACC_KEY_ID,
-            secretAccessKey: process.env.SCR_ACC_KEY
-        },
-        region: process.env.OSS_REGION,
-        endpoint: process.env.OSS_ENDPOINT
-    },
-    email: {
-        hostname: process.env.HOST_MAIL,
-        username: process.env.USER_MAIL,
-        password: process.env.PASS_MAIL,
-        port: process.env.PORT_MAIL
-    },
     secret: process.env.SECRET
 };
